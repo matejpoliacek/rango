e73f67dc201049b37a34285ce3dd06bba40b232d
Matej Poliacek <1103577p@student.gla.ac.uk>
2017-01-26 10:13:33 +0000

    end chapter 4 with exercises

--- rango/views.py
+++ rango/views.py
@@ -4,3 +4,7 @@ from django.http import HttpResponse
 def index(request):
         context_dict = {'boldmessage': "Crunchy, creamy, cookie, candy, cupcake!"}
         return render(request, 'rango/index.html', context=context_dict)
+
+def about(request):
+        context_dict = {'aboutmessage': "This tutorial has been put together by Matej Poliacek"}
+        return render(request, 'rango/about.html', context=context_dict)
