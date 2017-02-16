9f0a4e52d3cfa633a9b5160abba9df985c31b769
Matej Poliacek <poliacek.matej@gmail.com>
2017-02-16 00:52:41 +0000

    end chapter 10.6

--- rango/views.py
+++ rango/views.py
@@ -4,15 +4,51 @@ from rango.models import Category, Page
 from rango.forms import CategoryForm, PageForm, UserForm, UserProfileForm
 from django.contrib.auth import authenticate, login, login_required, logout
 from django.core.urlresolvers import reverse
+from datetime import datetime
+
+
+def get_server_side_cookie(request, cookie, default_val=None):
+    val = request.session.get(cookie)
+    if not val:
+        val = default_val
+    return val
+
+
+def visitor_cookie_handler(request, response):
+    visits = int(request.COOKIES.get('visits', '1'))
+    last_visit_cookie = request.COOKIES.get(request,
+                                            'last_visit',
+                                            str(datetime.now()))
+    last_visit_time = datetime.strptime(last_visit_cookie[:-7],
+                                        '%Y-%m-%d %H:%M:%S')
+
+    if (datetime.now() - last_visit_time).days > 0:
+        visits = visits + 1
+        request.session['last_visit'] = str(datetime.now())
+    else:
+        visits = 1
+        request.session['last_visit'] = last_visit_cookie)
+
+    request.session['visits'] = visits
+
 
 def index(request):
+    request.session.set_test_cookie()
     category_list = Category.objects.order_by('-likes')[:5]
     page_list = Page.objects.order_by('-views')[:5]
     context_dict = {'categories': category_list, 'pages': page_list}
-    return render(request, 'rango/index.html', context=context_dict)
 
+    visitor_cookie_handler(request)
+    context_dist['vitits'] = request.session['visits']
 
+    response = render(request, 'rango/index.html', context=context_dict)
+    return response
+
+        
 def about(request):
+    if request.session.test_cookie_worked()
+        print("TEST COOKIE WORKED!")
+        request.session.delete_test_cookie()
     context_dict = {'aboutmessage': "This tutorial has been put together by Matej Poliacek"}
     return render(request, 'rango/about.html', context=context_dict)
 
